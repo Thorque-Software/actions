a9d0caaeef56e187dd89c48ade88405c75c4e877
Lautaro <lautarotetamusa@gmail.com>
2026-04-06 00:24:14 -0300

    fix: do not need confirmation on install docker script

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -4,7 +4,7 @@ sudo apt remove $(dpkg --get-selections docker.io docker-compose docker-compose-
 
 # Add Docker's official GPG key:
 sudo apt update
-sudo apt install ca-certificates curl
+sudo apt install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -20,7 +20,7 @@ EOF
 
 sudo apt update
 
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo groupadd docker
 
